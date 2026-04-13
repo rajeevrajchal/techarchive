e067d933ca714c55b5e7a59f3e7c151739e3c93d
Rajeev Rajchal <rajeev@setsnail.com>
2026-04-13 17:39:22 +0200

    update

---
+++ src/lib/modules/exhibition/exhibition-content.svelte
@@ -0,0 +1,19 @@
+<div class="exhibition-content">
+	<div class="item">item</div>
+</div>
+
+<style>
+	.exhibition-content {
+		flex: 1;
+		width: 100%;
+		height: 100%;
+
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.item {
+		border: 1px solid red;
+	}
+</style>
--- src/routes/exhibition/+page.svelte
+++ src/routes/exhibition/+page.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
 	import ExhibitionBottomMenu from '@modules/exhibition/exhibition-bottom-menu.svelte';
+	import ExhibitionContent from '@modules/exhibition/exhibition-content.svelte';
 	import ExhibitionMenu from '@modules/exhibition/exhibition-menu.svelte';
 </script>
 
 <section>
 	<ExhibitionMenu />
-	<div class="exhibition-content">
-		<div class="item">the item</div>
-	</div>
+	<ExhibitionContent />
+
 	<ExhibitionBottomMenu />
 </section>
 
@@ -18,18 +18,5 @@
 		position: relative;
 		padding-inline: calc(env(--space-lg) * 2);
 		z-index: 2;
-	}
-
-	.exhibition-content {
-		width: 100%;
-		height: 100%;
-
-		display: flex;
-		align-items: center;
-		justify-content: center;
-	}
-
-	.item {
-		border: 1px solid red;
 	}
 </style>
